2c1eeb8309f3de5cb50ad18eae609f39c9f0139e
YvanGhomman <yvan.ghomman.yg@gmail.com>
2021-10-28 15:19:50 +0200

    set up host, user, password and db

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    HOST: process.env.DB_HOST /* "eu-cdbr-west-01.cleardb.com" */,
-    USER: process.env.DB_USER /* "b6017d09c3b5cf" */,
-    PASSWORD: process.env.DB_PASSW /* "d9d84aed" */,
-    DB: process.env.DB_DB /* "heroku_94b94d867de9aff" */
+    HOST: /* process.env.DB_HOST */ "eu-cdbr-west-01.cleardb.com",
+    USER: /* process.env.DB_USER */ "b6017d09c3b5cf",
+    PASSWORD: /* process.env.DB_PASSW */ "d9d84aed",
+    DB: /* process.env.DB_DB  */"heroku_94b94d867de9aff"
   };
